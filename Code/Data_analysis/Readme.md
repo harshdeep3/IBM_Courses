752c7a0800fb6086a833dd597da51019d5bf6ffe
harshdeep3 <harshdeep366@gmail.com>
2024-01-20 14:44:57 +0000

    updating readme with more notes. and updating code to include data preprocessing

--- Code/Data_analysis/Readme.md
+++ Code/Data_analysis/Readme.md
@@ -15,3 +15,21 @@
     * replace it based on other functions
 
 * Leave it as missing data
+
+### Normalising 
+
+* Simple Feature scaling 
+    * dividing each value by the maximum value for the feature 
+    * n_new = x_old/x_max
+    * ranges between 0 and 1
+
+* Min-Max
+    * takes each value x_old substracted from the minimum value for a feature and divides by the range of the feature
+    * n_new = (x_old - x_min)/(x_max - x_min)
+    * ranges between 0 and 1
+
+* z-score (standard score)
+    *   subtract the Mu from each value divided by the standard deviation
+        * Mu is the average of the feature
+    *   x_new = (x_old - x_mu)/ standard_deviation
+    * ranges between -3 and 3 
--- Code/Data_analysis/data_handling.py
+++ Code/Data_analysis/data_handling.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     # file path to load
     path = "Code/Data_analysis/data/file.csv"
-    data = pd.read_csv(path)
+    df = pd.read_csv(path)
     
     headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num-of-doors","body-style", 
                "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type", 
@@ -15,23 +15,54 @@ if __name__ == "__main__":
                "peak-rpm","city-mpg","highway-mpg","price"]
     
     # adding the columns headers
-    data.columns = headers
+    df.columns = headers
     
     # replacing ? from nan
-    data = data.replace('?', np.nan)
-    
-    # get top 5 rows
-    print(data.head())
+    df = df.replace('?', np.nan)
     
     # drop missing values 
     # inplace = True -> writes the results back into the dataframe
     # axis = 0 drops the entire row ---- axis =1 drops the entire column
-    data.dropna(subset=["price"], axis=0, inplace=True)
+    df.dropna(subset=["price"], axis=0, inplace=True)
     
     # replaces the str Nan with a float/int and converts the number as str to int 
-    data["normalized-losses"] = data['normalized-losses'].replace('NaN', pd.NA).fillna(0).astype(int)
+    df["normalized-losses"] = df['normalized-losses'].replace('NaN', pd.NA).fillna(0).astype(int)
     
     # replacing the data with the average 
-    mean = data['normalized-losses'].mean()
-    data["normalized-losses"].replace(np.nan, mean)
+    mean = df['normalized-losses'].mean()
+    df["normalized-losses"].replace(np.nan, mean)
     
+    ####################################################
+    ################# Normalisation ####################
+    
+    # Min-Max  
+    df['length'] = (df['length'] - df['length'].min())/ (df['length'].max() - df['length'].min())
+    
+    # Z-score    
+    df['width'] = (df['width'] - df['width'].mean())/ df['width'].std()
+    
+    ####################################################
+    ##################### Binning ######################
+
+    # convert str to int
+    df['price'] = df['price'].astype(int)
+    
+    # create 4 bins
+    bins = np.linspace(min(df['price']), max(df['price']), 4)
+    group_names = ['Low', 'Medium', 'High']
+    
+    # cut data to bins
+    df['price-binned'] = pd.cut(df['price'], bins, labels=group_names, include_lowest=True)
+    
+    ####################################################
+    ################# One_hot encoding #################
+    
+    one_hot_encodding = pd.get_dummies(df['fuel-type'])
+    
+    df = df.assign(Diesel=one_hot_encodding['diesel'].astype(int),
+                   Gas=one_hot_encodding['gas'].astype(int))
+    
+    
+    
+    # get top 5 rows
+    print(df.head())
